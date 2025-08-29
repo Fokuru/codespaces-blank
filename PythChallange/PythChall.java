82c20d6c7d7c718c3378c6127b45bcfb18aed42b
Fokuru <148640686+Fokuru@users.noreply.github.com>
2025-08-29 16:40:00 +0000

    Changed

--- PythChallange/PythChall.java
+++ PythChallange/PythChall.java
@@ -1,21 +1,24 @@
 public class PythChall {
     public static void main(String[] args) {
-        String alph = "abcdefghijklmnopqrstuvwxyz";
         String theText = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj";
-        System.out.println(moveLetter(alph, 2, theText));
+        System.out.println(moveLetter(2, theText));
     }
 
-    public static String moveLetter(String firstAlph, int moveNum, String imput){
+    public static String moveLetter(int moveNum, String imput){
+        String firstAlph = "abcdefghijklmnopqrstuvwxyz";
         String output = "";
-        String secondAlph = firstAlph.subString(moveNum+1) + firstAlph.subString(0, moveNum+1);
-        for (int i = 0; i < imput.length; i++){
-            int index = 0;
-                for (int k = 0; k < firstAlph.length; k++) {
-                    if (imput.subString(i, i+1).equals(secondAlph.subString(k, k+1))){
-                        output = output + firstAlph.subString(k, k+1);
-                    }
+        String secondAlph = firstAlph.substring(moveNum) + firstAlph.substring(0, moveNum);
+        for (int i = 0; i < imput.length(); i++){
+            if (imput.substring(i, i+1).equals(" ")){
+                output = output + " ";
+            } else {
+            for (int k = 0; k < firstAlph.length(); k++) {
+                if (imput.substring(i, i+1).equals(firstAlph.substring(k, k+1))){
+                    output = output + secondAlph.substring(k, k+1);
                 }
+            }
         }
+    }
         return output;
     }
 }
